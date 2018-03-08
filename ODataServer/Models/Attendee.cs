4694f19d1c8449f772c1e90460feb055d8131119
shivani borkar <shivani.borkar@yahoo.com>
2018-03-08 20:52:37 +0100

    a

---
+++ ODataServer/Models/Attendee.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ODataServer.Models
+{
+    public class Attendee
+    {
+        public int AttendeeId { get; set; }
+        public string name { get; set; }
+        public int age { get; set; }
+
+        public int dummyProperty { get; set; }
+        public int dummyProperty1 { get; set; }
+        public int dummyProperty2 { get; set; }
+
+    }
+}
